41aa025b32a83aecc45fdea1c849df9f025d7dc8
Evgeny <drjackq@yandex.ru>
2023-03-02 15:08:54 +0300

    Добавили EX1 Задача

--- EX1/Program.cs
+++ EX1/Program.cs
@@ -1,1 +1,31 @@
-﻿int a = 23;
+﻿// Напишите программу, которая принимает на вход координаты точки(Х и У),
+// причем Х не равно 0 и У не равно 0 и выдает номер четверти плоскости, 
+// в которой находится эта точка.
+
+Console.Clear();
+Console.Write("Введите Х: ");
+int X = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите У: ");
+int Y = int.Parse(Console.ReadLine()!);
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("1");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("2");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("3");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("4");
+}
+else if (X == 0 || Y == 0)
+{
+    Console.WriteLine("На оси");
+}
