53ab396472bf66ec909c06a1901b06396d850030
Evgeny <drjackq@yandex.ru>
2023-03-02 16:51:11 +0300

    Задача 22

---
+++ EX4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 22 
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+Console.Write($"{N} -> ");
+while (count <= N)
+{
+    Console.Write($"{Math.Pow(count,2)}, ");
+    count++;
+}
